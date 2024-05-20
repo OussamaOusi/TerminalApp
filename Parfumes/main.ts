bb317d4433d2fbf65dc5adcbd0ebb25385646ac3
OussamaOusi <oussama.amezian@hotmail.com>
2024-05-20 23:42:34 +0000

    test

--- Parfumes/main.ts
+++ Parfumes/main.ts
@@ -4,83 +4,43 @@ import {
 } from "./interfaces"
 import { MongoClient } from 'mongodb';
 
-const uri = "mongodb+srv://oussamaousi:TesterBester123@mongo-cluster-oussama.ylvexkn.mongodb.net/"; // Fill in your MongoDB connection string here
+const uri = "mongodb+srv://oussamaousi:MongoDB123@oussama-cluster.blxpfbi.mongodb.net/";
 const client = new MongoClient(uri);
 
 const app = express();
-
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.static("public"));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
-const fragrances: Fragrances[] = [];
+let fragrancesData: Fragrances[] = [];
 
-app.get("/", (req, res) => {
-    res.sendFile("index.html", { root: __dirname + "/views" });
-});
-
-async function writeToMongoDB(data: Fragrances[]) {
-    try {
-        await client.connect();
-        const database = client.db('fragrancesDB');
-        const collection = database.collection('fragrancesCollection');
-        await collection.insertMany(data);
-        console.log("Data successfully written to MongoDB.");
-    } catch (error) {
-        console.error("Error writing data to MongoDB:", error);
-    } finally {
-        await client.close();
-    }
-}
 app.get("/", async (req, res) => {
-    try {
-        await client.connect();
-        const database = client.db('fragrancesDB');
-        const collection = database.collection('fragrancesCollection');
-        const result = await collection.find({}).toArray();
-        if (result.length === 0) {
-            const response = await fetch("https://raw.githubusercontent.com/OussamaOusi/TerminalApp/main/parfum.json");
-            const data = await response.json();
-            await writeToMongoDB(data);
-            fragrances.push(...data);
-        } else {
-            fragrances.push(...result.map(item => ({
-                id: item.id,
-                name: item.name,
-                description: item.description,
-                age: item.age,
-                has_website: item.has_website,
-                releasedate: new Date(item.releasedate),
-                profile_image_url: item.profile_image_url,
-                category: item.category,
-                price: item.price,
-                season: item.season,
-                details: {
-                    id: item.details.id,
-                    notes: {
-                        top_notes: item.details.notes.top_notes,
-                        middle_notes: item.details.notes.middle_notes,
-                        base_notes: item.details.notes.base_notes
-                    }
-                }
-            })));
-        }
-        res.sendFile("index.html", { root: __dirname + "/views" });
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).send("Internal Server Error");
-    } finally {
-        await client.close();
-    }
+    res.render("index", { fragrances: fragrancesData });
 });
-app.listen(app.get('port'), async () => {
+async function main() {
     try {
-        const response = await fetch("https://raw.githubusercontent.com/OussamaOusi/TerminalApp/main/parfum.json");
-        const parfume = await response.json();
-        fragrances.push(...parfume);
-    } catch (error) {
-        console.log(error);
+      await client.connect();
+      console.log("Connected to MongoDB");
+      const database = client.db("fragrancesDB");
+      const fragrancesCheck = await database.collection("fragrancesCollection").findOne({});
+  
+      if (!fragrancesCheck) {
+        const fragrancesResponse = await fetch('https://raw.githubusercontent.com/OussamaOusi/TerminalApp/main/parfum.json');
+        const fragrancesData = await fragrancesResponse.json();
+        fragrancesData.push(...fragrancesData);
+  
+        await database.collection("fragrancesCollection").insertMany(fragrancesData);
+      }
+
+      fragrancesData = await database.collection("fragrancesCollection").find<Fragrances>({}).toArray();
+  
+      app.listen(app.get('port'), async () => {
+        console.log(`Server is running at http://localhost:${app.get('port')}`);
+      }
+      );
+    } catch (err) {
+      console.error(err);
     }
-    console.log('Server started on port ' + app.get('port'));
-});
+  }
+  main();
